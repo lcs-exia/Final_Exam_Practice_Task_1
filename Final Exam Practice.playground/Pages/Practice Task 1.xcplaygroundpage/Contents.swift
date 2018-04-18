2435665635b33b7cc44843aaffd5fbe82372b9ec
Eric Xia <exia@lcs.on.ca>
2018-04-18 11:40:06 -0400

    Finished blur

--- Final Exam Practice.playground/Pages/Practice Task 1.xcplaygroundpage/Contents.swift
+++ Final Exam Practice.playground/Pages/Practice Task 1.xcplaygroundpage/Contents.swift
@@ -27,11 +27,22 @@ let canvas = Canvas(width: 400, height: 600)
 // Begin your solution here...
 
 // REMINDER: here's how to create a square with a fill
-canvas.fillColor = Color(hue: 56, saturation: 7, brightness: 56, alpha: 100)
-canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 50, height: 50)
-
+canvas.fillColor = Color.init(hue: 191, saturation: 100, brightness: 85, alpha: 100)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 400, height: 600)
 // REMINDER: here's how to create text in a certain location
-canvas.drawText(message: "blur", size: 96, x: 100, y: 150, kerning: 0)
+canvas.textColor = Color.init(hue: 0, saturation: 0, brightness: 0, alpha: 21)
+for x in stride(from: 310, to: -60, by: -10){
+    canvas.drawText(message: "blur", size: 200, x: 10, y: x, kerning: 0)
+}
+canvas.textColor = Color.black
+canvas.drawText(message: "blur", size: 200, x: 10, y: 280, kerning: 0)
+canvas.drawText(message: "saturday", size: 10, x: 20, y: 560, kerning: -0.5)
+canvas.drawText(message: "october 16 1993 / 8 pm", size: 10, x: 20, y: 548, kerning: -0.5)
+canvas.drawText(message: "mandela hall", size: 10, x: 150, y: 560, kerning: -0.5)
+canvas.drawText(message: "at university of sussex", size: 10, x: 150, y: 548, kerning: -0.5)
+canvas.drawText(message: "special guest", size: 10, x: 280, y: 560, kerning: -0.5)
+canvas.drawText(message: "salad, plus  my life story", size: 10, x: 280, y: 550, kerning: -0.5)
+canvas.drawText(message: "€6.50 / €5.50 advance", size: 10, x: 280, y: 530, kerning: -0.5)
 // HINT: You can use the kerning property to change the distance between letters
 //       Try adjusting the value to positive or negative values. What happens?
 
